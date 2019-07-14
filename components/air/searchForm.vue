10482d03f0c04d1288247cdea7bbdf2f6ce4d4d1
itcast-hsian <43436182+itcast-hsian@users.noreply.github.com>
2019-07-14 11:18:36 +0800

    机票首页布局

---
+++ components/air/searchForm.vue
@@ -0,0 +1,243 @@
+<template>
+    <div class="search-form">
+
+        <!-- 头部tab切换 -->
+        <el-row type="flex" class="search-tab">
+            <span v-for="(item, index) in tabs" :key="index"
+            @click="handleSearchTab(item, index)"
+            :class="{active: index === currentTab}">
+                <i :class="item.icon"></i>{{item.name}}
+            </span>
+        </el-row>
+
+        <el-form class="search-form-content" ref="form" label-width="80px">
+            <el-form-item label="出发城市">
+
+                <!-- fetch-suggestions: 当输入框输入值时候触发搜索建议事件 -->
+                <!-- select：选中下拉框时候触发，参数是选中的参数对象 -->
+                <el-autocomplete
+                :fetch-suggestions="queryDepartSearch"
+                placeholder="请搜索出发城市"
+                @select="handleDepartSelect"
+                class="el-autocomplete"
+                v-model="form.departCity"
+                ></el-autocomplete>
+
+            </el-form-item>
+            <el-form-item label="到达城市">
+
+                <el-autocomplete
+                :fetch-suggestions="queryDestSearch"
+                placeholder="请搜索到达城市"
+                @select="handleDestSelect"
+                class="el-autocomplete"
+                v-model="form.destCity"
+                ></el-autocomplete>
+
+            </el-form-item>
+            <el-form-item label="出发时间">
+                <!-- change 用户确认选择日期时触发 -->
+                <el-date-picker type="date" 
+                placeholder="请选择日期" 
+                style="width: 100%;"
+                @change="handleDate"
+                v-model="departDate">
+                </el-date-picker>
+            </el-form-item>
+            <el-form-item label="">
+                <el-button style="width:100%;" 
+                type="primary" 
+                icon="el-icon-search"
+                @click="handleSubmit">
+                    搜索
+                </el-button>
+            </el-form-item>
+            <div class="reverse">
+                <span @click="handleReverse">换</span>
+            </div>
+        </el-form>  
+      </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            tabs: [
+                {icon: "iconfont icondancheng", name: "单程"},
+                {icon: "iconfont iconshuangxiang", name: "往返"}
+            ],
+            currentTab: 0,
+            form: {
+                departCity: "", // 出发城市
+                destCity: "",   // 到达城市
+                departDate: "", // 出发日期
+
+                departCode: "", // 出发城市代码
+                destCode: ""    // 到达城市代码
+            }
+        }
+    },
+    methods: {
+        // tab切换时触发
+        handleSearchTab(item, index){
+            
+        },
+        
+        // 输入文字时候触发
+        // value是输入框的值
+        // cb 是回调函数,
+        queryDepartSearch(value, cb){
+
+            // 回调函数中的参数必须是一个数组
+            // 数组中每一项必须是一个对象，对象中必须包含value属性
+            cb([
+                // { value: 1 },
+                // { value: 2 } 
+            ])
+        },
+
+        // 目标城市输入框获得焦点时触发
+        // value 是选中的值，cb是回调函数，接收要展示的列表
+        queryDestSearch(value, cb){
+            cb([
+                {value: 1},
+                {value: 2},
+                {value: 3},
+            ]);
+        },
+       
+        // 出发城市下拉选择时触发
+        handleDepartSelect(item) {
+            
+        },
+
+        // 目标城市下拉选择时触发
+        handleDestSelect(item) {
+            
+        },
+
+        // 确认选择日期时触发
+        handleDate(value){
+           
+        },
+
+        // 触发和目标城市切换时触发
+        handleReverse(){
+            
+        },
+
+        // 提交表单是触发
+        handleSubmit(){
+           
+        }
+    },
+    mounted() {
+       
+    }
+}
+</script>
+
+<style scoped lang="less">
+.search-form{
+    border:1px #ddd solid;
+    border-top:none;
+    width:360px;
+    height:350px;
+    box-sizing: border-box;
+}
+
+.search-tab{
+  span{
+    display: block;
+    flex:1;
+    text-align: center;
+    height:48px;
+    line-height: 42px;
+    box-sizing: border-box;
+    border-top:3px #eee solid;
+    background:#eee;
+  }
+
+  .active{
+    border-top-color: orange;
+    background:#fff;
+  }
+
+  i{
+    margin-right:5px;
+    font-size: 18px;
+
+    &:first-child{
+      font-size:16px;
+    }
+  }
+}
+
+.search-form-content{
+  padding:15px 50px 15px 15px;
+  position: relative;
+
+  .el-autocomplete{
+    width: 100%;
+  }
+}
+
+.reverse{
+  position:absolute;
+  top: 35px;
+  right:15px;
+
+  &:after,&:before{
+      display: block;
+      content: "";
+      position: absolute;
+      left:-35px;
+      width:25px;
+      height:1px;
+      background:#ccc;
+  }
+
+  &:after{
+      top:0;
+    }
+
+    &:before{
+      top:60px;
+    }
+
+  span{
+    display: block;
+    position:absolute;
+    top: 20px;
+    right:0;
+    font-size:12px;
+    background: #999;
+    color:#fff;
+    width:20px;
+    height:20px;
+    line-height: 18px;
+    text-align: center;
+    border-radius: 2px;
+    cursor: pointer;
+
+    &:after,&:before{
+      display: block;
+      content: "";
+      position: absolute;
+      left:10px;
+      width:1px;
+      height:20px;
+      background:#ccc;
+    }
+
+    &:after{
+      top:-20px;
+    }
+
+    &:before{
+      top:20px;
+    }
+  }
+}
+</style>
