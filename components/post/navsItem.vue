977353618e20f33fb7a8ff78b3b78e9ae9768e1d
itcast-hsian <43436182+itcast-hsian@users.noreply.github.com>
2019-07-20 11:30:29 +0800

    组件递归

---
+++ components/post/navsItem.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="div">
+        <div v-for="(item, index) in  data" :key="index">
+            {{ item.title }}
+
+            <NavsItem :data="item.children" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    // 1.先命名name, 在当前组件内可以通过name自己调用自己
+    name: "NavsItem",
+    props: ["data"]
+}
+</script>
+
+<style scoped>
+    .div  div{
+        margin-left:20px;
+    }
+
+    
+</style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,59 +1,66 @@
 <template>
-  <div>
-     组件递归
+  <div style="padding:50px;">
+     <h1 style="margin-bottom:50px;">组件递归</h1>
 
+    <NavsItem :data="navs"/>
 
   </div>
 </template>
 
 <script>
 
+import NavsItem from "@/components/post/navsItem";
+
 export default {
-  data(){
-      return {
-          navs: [
-            {
-                title: "衣服",
-                children: [
-                    { 
-                        title: "男装",
-                        children: [
-                            { 
-                                title: "T恤",
-                                children: [ {title: "短袖"}, { title: "长袖" } ]
-                            }
-                        ]
-                    },
-                    { 
-                        title: "女装",
-                        children: [
-                            {
-                                title: "裙子",
-                                children: [
-                                    { title: "连衣裙" },
-                                    { title: "短裙" }
-                                ]
-                            }
-                        ]
-                    }
-                ]
-            },
-
-            {
-                title: "电器",
-                children: [
-                    { title: "电视", children: [
-                        { title: "液晶电视" },
-                        { title: "普通电视"}
-                    ] },
-                    { title: "冰箱" },
-                    { title: "空调" },
-                ]
-            }
-
-          ]
-      }
-  }
+    components: {
+        NavsItem,
+    },
+    data(){
+        return {
+            navs: [
+                {
+                    title: "衣服",
+                    children: [
+                        { 
+                            title: "男装",
+                            children: [
+                                { 
+                                    title: "T恤",
+                                    children: [ 
+                                        {title: "短袖"}, 
+                                        { title: "长袖" }
+                                     ]
+                                }
+                            ]
+                        },
+                        { 
+                            title: "女装",
+                            children: [
+                                {
+                                    title: "裙子",
+                                    children: [
+                                        { title: "连衣裙" },
+                                        { title: "短裙" }
+                                    ]
+                                }
+                            ]
+                        }
+                    ]
+                },
+                {
+                    title: "电器",
+                    children: [
+                        { title: "电视", children: [
+                            { title: "液晶电视" },
+                            { title: "普通电视"}
+                        ] },
+                        { title: "冰箱" },
+                        { title: "空调" },
+                    ]
+                }
+            ]
+        }
+    }
 }
 </script>
 
